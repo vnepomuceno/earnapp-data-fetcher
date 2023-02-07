66d785d9ea32870067b482b8f72e954a3e3c92b7
Valter Nepomuceno <valter.nep@gmail.com>
2023-02-07 20:17:32 +0000

    Add dependencies for running EarnApp API calls

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+import os
+
+from dotenv import load_dotenv
+from pyEarnapp import EarnApp
+
+load_dotenv()
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def run_earnapp():
+    AUTH = os.environ.get('EARNAPP_OAUTH_REFRESH_TOKEN')
+
+    api = EarnApp(AUTH)
+    print("Successfully called EarnApp API")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    run_earnapp()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
